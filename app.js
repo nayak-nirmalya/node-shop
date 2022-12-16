34b44961b92cf1b8c1dc258532bc6ae8e8b64d54
Nirmalya Nayak <n.nayak263@gmail.com>
2022-12-16 12:08:48 +0530

    connect-mongodb-session to save session in mongodb

--- app.js
+++ app.js
@@ -4,11 +4,19 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const session = require('express-session')
+const MongoDBStore = require('connect-mongodb-session')(session)
 
 const errorController = require('./controllers/error')
 const User = require('./models/user')
 
+const MONGODB_URI =
+  'mongodb+srv://nirmalya:nirmalya@cluster.a9tjk7u.mongodb.net/shop'
+
 const app = express()
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'session',
+})
 
 app.set('view engine', 'ejs')
 app.set('views', 'views')
@@ -24,6 +32,7 @@ app.use(
     secret: 'my-secret',
     resave: false,
     saveUninitialized: false,
+    store: store,
   }),
 )
 
@@ -43,7 +52,7 @@ app.use(authRoutes)
 app.use(errorController.get404)
 
 mongoose
-  .connect('mongodb+srv://nirmalya:nirmalya@cluster.a9tjk7u.mongodb.net/shop')
+  .connect(MONGODB_URI)
   .then((result) => {
     User.findOne().then((user) => {
       if (!user) {
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,9 @@
 exports.getLogin = (req, res, next) => {
-  // const isLoggedIn = req.get('Cookie').split('=')[1]
-  // console.log(isLoggedIn)
+  console.log(req.session.isLoggedIn)
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: false,
+    isAuthenticated: req.session.isLoggedIn,
   })
 }
 
