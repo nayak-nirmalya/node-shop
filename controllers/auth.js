7ed8fe0f24ca42f413db6e78e1a8ca2bdc7c1ad8
Nirmalya Nayak <n.nayak263@gmail.com>
2022-12-18 10:07:54 +0530

    added postReset controller to send token to email

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const crypto = require('crypto')
+
 const bcrypt = require('bcryptjs')
 const nodemailer = require('nodemailer')
 const sendgridTransport = require('nodemailer-sendgrid-transport')
@@ -124,5 +126,40 @@ exports.getReset = (req, res, next) => {
     path: '/reset',
     pageTitle: 'Reset Password',
     errorMessage: message,
+  })
+}
+
+exports.postReset = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.error(err)
+      return res.redirect('/reset')
+    }
+    const token = buffer.toString('hex')
+    User.findOne({ email: req.body.email })
+      .then((user) => {
+        if (!user) {
+          req.flash('error', 'No Account with that E-Mail found.')
+          return res.redirect('/reset')
+        }
+        user.resetToken = token
+        user.resetTokenExpiration = Date.now() + 3_600_000
+        return user.save()
+      })
+      .then((result) => {
+        res.redirect('/')
+        transporter.sendMail({
+          to: req.body.email,
+          from: 'gobindathedog@gmail.com',
+          subject: 'Password Reset',
+          html: `
+            <p>You requested password reset.</p>
+            <p>Click this <a herf="http://localhost:3000/reset/${token}>link</a> to set new password.</p>
+          `,
+        })
+      })
+      .catch((err) => {
+        console.error(err)
+      })
   })
 }
