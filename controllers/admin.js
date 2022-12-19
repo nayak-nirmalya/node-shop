73707d28fb852959452fcf88ecfd70a27b7d48e7
Nirmalya Nayak <n.nayak263@gmail.com>
2022-12-19 12:24:41 +0530

    added new view for error handeling

--- controllers/admin.js
+++ controllers/admin.js
@@ -23,7 +23,7 @@ exports.postAddProduct = (req, res, next) => {
   if (!errors.isEmpty()) {
     return res.status(422).render('admin/edit-product', {
       pageTitle: 'Add Product',
-      path: '/admin/edit-product',
+      path: '/admin/add-product',
       editing: false,
       hasError: true,
       product: {
@@ -51,7 +51,7 @@ exports.postAddProduct = (req, res, next) => {
       res.redirect('/admin/products')
     })
     .catch((err) => {
-      console.log(err)
+      res.redirect('/500')
     })
 }
 
