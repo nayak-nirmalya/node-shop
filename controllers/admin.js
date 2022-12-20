3a6b19697e95941d80533d5c49a13fb6cb83ff35
Nirmalya Nayak <n.nayak263@gmail.com>
2022-12-20 20:10:58 +0530

    manipulating client side js

--- controllers/admin.js
+++ controllers/admin.js
@@ -171,23 +171,21 @@ exports.getProducts = (req, res, next) => {
     })
 }
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId
   Product.findById(prodId)
     .then((product) => {
       if (!product) {
         return next(new Error('Product Not Found!'))
       }
       fileHelper.deleteFile(product.imageUrl)
-      Product.deleteOne({ _id: prodId, userId: req.user._id })
+      return Product.deleteOne({ _id: prodId, userId: req.user._id })
     })
     .then(() => {
       console.log('DESTROYED PRODUCT')
-      res.redirect('/admin/products')
+      res.status(200).json({ message: 'Success' })
     })
     .catch((err) => {
-      const error = new Error(err)
-      error.httpStatusCode = 500
-      return next(error)
+      res.status(500).json({ message: 'Deleting Product Failed!' })
     })
 }
